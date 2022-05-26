c24e4a35e286818e5f18bdd33a3293b60298830e
Michael Schroeder <magicalmikely@gmail.com>
2022-05-26 12:25:04 -0700

    Testing files created

---
+++ app/src/test/java/REST_API_TakeHome/ProductApplicationTest.java
@@ -0,0 +1,11 @@
+package REST_API_TakeHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
