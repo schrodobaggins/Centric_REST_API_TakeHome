3b90b7ef8a0f7cd558a83f1431f511b67034c2fb
Michael Schroeder <magicalmikely@gmail.com>
2022-05-26 12:20:46 -0700

    ExceptionHandler 400 codes added

--- app/src/main/java/REST_API_TakeHome/exceptions/ResponseExceptionHandler.java
+++ app/src/main/java/REST_API_TakeHome/exceptions/ResponseExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
@@ -25,13 +24,33 @@ import javax.servlet.http.HttpServletRequest;
 public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<Object> handleInternalServerError(HttpServletRequest req, Exception ex) {
-        Map<String, Object> response = new HashMap<>();
-        response.put("timestamp", Instant.now().toString());
-        response.put("status", 500);
-        response.put("message", "Unhandled exception caught");
+    protected ResponseEntity<Object> handleInternalServerError(HttpServletRequest request, Exception exception) {
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("timestamp", Instant.now().toString());
+        responseMap.put("status", 500);
+        responseMap.put("message", "Unhandled exception caught");
 
-        return new ResponseEntity<Object>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<Object>(responseMap, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = JSONTypeException.class)
+    public ResponseEntity<Map<String, Object>> JsonTypeException(HttpServletRequest request, JSONTypeException exception) {
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("timestamp", Instant.now().toString());
+        responseMap.put("status", 400);
+        responseMap.put("message", exception.getMessage());
+        responseMap.put("path", request.getRequestURI());
+        return new ResponseEntity<Map<String, Object>>(responseMap, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable (HttpMessageNotReadableException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("timestamp", Instant.now().toString());
+        responseMap.put("status", 400);
+        responseMap.put("message", "JSON contains invalid input");
+
+        return new ResponseEntity<Object>(responseMap, HttpStatus.BAD_REQUEST);
     }
 
 }
